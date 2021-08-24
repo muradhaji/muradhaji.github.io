3ef6cc62421345c8a9acfa6d64d04f3e96259517
Murad Hajiyev <murad.haciyev00@gmail.com>
2021-08-24 20:41:05 +0400

    starting to mobile responsive design

--- js/stores.js
+++ js/stores.js
@@ -1,8 +1,21 @@
-const API_URL = "http://192.168.0.144:5555/api/"
+// const API_URL = "http://192.168.0.144:5555/api/";
+// const API_URL = "http://comingsoon.pythonanywhere.com/api/";
+const API_URL = "https://comingsoon.pythonanywhere.com/api/";
+
+const showNotification = (type, text) => {
+  Toastify({
+    text,
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    className: "custom-toast toast-type-" + type,
+  }).showToast();
+};
 
 const showStoreInfo = (storeId) => {
-  for(let i = 1 ; i < 4 ; i++) {
-    if(i === storeId) {
+  for (let i = 1; i < 4; i++) {
+    if (i === storeId) {
       activateNavigation(i);
     } else {
       hideNavigation(i);
@@ -14,73 +27,106 @@ const activateNavigation = (id) => {
   let store = document.getElementById(`store${id}`);
   let storelogo = document.getElementById(`storelogo${id}`);
   let storelogoactive = document.getElementById(`storelogo${id}active`);
-  let navtext = document.getElementById(`storenav${id}`)
+  let navtext = document.getElementById(`storenav${id}`);
   store.style.display = "flex";
   storelogo.style.display = "none";
   storelogoactive.style.display = "block";
-  navtext.style.color = "var(--primary-color)"
-}
+  navtext.style.color = "var(--primary-color)";
+};
 
 const hideNavigation = (id) => {
   let store = document.getElementById(`store${id}`);
   let storelogo = document.getElementById(`storelogo${id}`);
   let storelogoactive = document.getElementById(`storelogo${id}active`);
-  let navtext = document.getElementById(`storenav${id}`)
+  let navtext = document.getElementById(`storenav${id}`);
   store.style.display = "none";
   storelogo.style.display = "block";
   storelogoactive.style.display = "none";
-  navtext.style.color = "var(--text-color)"
-}
+  navtext.style.color = "var(--text-color)";
+};
 
 const showMap = (id) => {
   const map = document.getElementById(`map${id}`);
   const preview = document.getElementById(`preview${id}`);
-  map.style.display = 'block';
-  preview.style.display = 'none';
-}
+  map.style.display = "block";
+  preview.style.display = "none";
+};
 
 const showImage = (id, url) => {
   const map = document.getElementById(`map${id}`);
   const preview = document.getElementById(`preview${id}`);
-  map.style.display = 'none';
-  preview.style.display = 'block';
+  map.style.display = "none";
+  preview.style.display = "block";
   preview.src = url;
-}
+};
 
 const isEmail = (text) => {
   const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return mailFormat.test(text);
-}
+};
 
 const isNumber = (text) => {
-  var numberFormat = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]{8,14}$/g
+  var numberFormat = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]{8,14}$/g;
   return numberFormat.test(text);
-}
+};
 
-const subscriptionForm = document.getElementById('subscriptionform');
-
-subscriptionForm.addEventListener('submit', (event) => {
+const subscriptionForm = document.getElementById("subscriptionform");
+subscriptionForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const text = event.target.numberormail.value;
-  console.log(text)
-  console.log(isEmail(text));
-  console.log(isNumber(text));
-})
+  let data = {};
+  if (isEmail(text)) {
+    Object.assign(data, { subMail: text });
+  } else if (isNumber(text)) {
+    Object.assign(data, { subNumber: text });
+  }
+  fetch(`${API_URL}`, {
+    method: "post",
+    headers: {
+      Authorization: "Basic " + btoa("davud:davud19"),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (res.ok) {
+        console.log(res);
+        showNotification("success", "You have subscribed successfully!");
+        event.target.numberormail.value = "";
+      } else {
+        showNotification("warning", "You have already subscribed!");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      showNotification("danger", "Someting went wrong!");
+    });
+});
 
-const orderForm = document.getElementById('orderform');
-
-orderForm.addEventListener('submit', (event) => {
+const orderForm = document.getElementById("orderform");
+orderForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const number = event.target.number.value;
   console.log(number);
-  fetch(`${API_URL}numbers/`, {
-    method: "post",
-    headers: {
-      "Authorization": 'Basic ' + btoa("davud:davud19"),
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({"subnNumber": number})
-  }).then(res => console.log(res))
-})
+  if (isNumber(number)) {
+    fetch(`${API_URL}numbers/`, {
+      method: "post",
+      headers: {
+        Authorization: "Basic " + btoa("davud:davud19"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ subnNumber: number }),
+    })
+      .then((res) => {
+        console.log(res);
+        showNotification("success", "Call ordered successfully!");
+        event.target.number.value = "";
+      })
+      .catch((error) => {
+        console.log(error);
+        showNotification("danger", "Someting went wrong!");
+      });
+  }
+});
 
 showStoreInfo(1);
